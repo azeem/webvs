080e55a05ccf707c5ae351779a98039898e24dce
azeem <kr00r4n@gmail.com>
2013-06-12 01:49:44 +0530

    EffectList WIP

--- src/core.js
+++ src/core.js
@@ -6,11 +6,10 @@
 function Webvs(options) {
     checkRequiredOptions(options, ["canvas", "components", "analyser"]);
     this.canvas = options.canvas;
-    this.components = options.components;
+    this.rootComponent = new EffectList(options.components);
     this.analyser = options.analyser;
 
     this._initGl();
-    this._initFrameBuffer();
 }
 extend(Webvs, Object, {
     _initGl: function() {
@@ -27,103 +26,23 @@ extend(Webvs, Object, {
             throw new Error("Couldnt get webgl context");
         }
     },
-    _initFrameBuffer: function() {
-        var gl = this.gl;
-        var framebuffer = gl.createFramebuffer();
-        var attachments = [];
-        for(var i = 0;i < 2;i++) {
-            var texture = gl.createTexture();
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.resolution.width, this.resolution.height,
-                          0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-
-            var renderbuffer = gl.createRenderbuffer();
-            gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
-            gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, this.resolution.width, this.resolution.height);
-
-            attachments[i] = {
-                texture: texture,
-                renderbuffer: renderbuffer
-            };
-        }
-        this.framebuffer = framebuffer;
-        this.frameAttachments = attachments;
-        this.currAttachment = 0;
-    },
-
-    _setFBAttachment: function() {
-        var gl = this.gl;
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.frameAttachments[this.currAttachment].texture, 0);
-        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this.frameAttachments[this.currAttachment].renderbuffer);
-    },
-
-    _getCurrentTextrue: function() {
-        return this.frameAttachments[this.currAttachment].texture;
-    },
-
-    _swapFBAttachment: function() {
-        this.currAttachment = (this.currAttachment + 1) % 2;
-        this._setFBAttachment();
-    },
 
     /**
      * Starts the animation
      */
     start: function() {
-        var gl = this.gl;
-        var components = this.components;
-        var copyComponent = new Copy();
+        var rootComponent = this.rootComponent
+        var promise = rootComponent.initComponent(this.gl, this.resolution, this.analyser);
 
-        // initialize all the components
-        var initPromises = [];
-        for(var i = 0;i < components.length;i++) {
-            var res = components[i].initComponent(gl, this.resolution, this.analyser);
-            if(res) {
-                initPromises.push(res);
-            }
-        }
-        copyComponent.initComponent(gl, this.resolution);
-
-        var self = this;
-        var first = true;
         var drawFrame = function() {
-            if(!self.analyser.isPlaying()) {
-                requestAnimationFrame(drawFrame);
-                return;
+            if(self.analyser.isPlaying()) {
+                rootComponent.updateComponent();
             }
-            // draw everything the temporary framebuffer
-            gl.bindFramebuffer(gl.FRAMEBUFFER, self.framebuffer);
-            gl.viewport(0, 0, self.resolution.width, self.resolution.height);
-            self._setFBAttachment();
-
-            for(var i = 0;i < components.length;i++) {
-                var component = components[i];
-                gl.useProgram(component.program);
-                if(component.swapFrame) {
-                    var oldTexture = self._getCurrentTextrue();
-                    self._swapFBAttachment();
-                    component.updateComponent(oldTexture);
-                } else {
-                    component.updateComponent();
-                }
-            }
-
-            // flip and copy the current texture to the screen
-            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-            gl.useProgram(copyComponent.program);
-            gl.viewport(0, 0, self.resolution.width, self.resolution.height);
-            copyComponent.updateComponent(self.frameAttachments[self.currAttachment].texture);
-
             requestAnimationFrame(drawFrame);
-        };
+        }
 
-        // start rendering when all the init promises are done
-        D.all(initPromises).then(function() {
+        // start rendering when the promise is done
+        promise.then(function() {
             requestAnimationFrame(drawFrame);
         });
     }
@@ -254,25 +173,5 @@ extend(Trans, ShaderComponent, {
         gl.drawArrays(gl.TRIANGLES, 0, 6);
     }
 });
-
-
-
-
-/**
- * Utility component that copies a texture to the screen
- * @constructor
- */
-function Copy() {
-    var fragmentSrc = [
-        "precision mediump float;",
-        "uniform sampler2D u_curRender;",
-        "varying vec2 v_texCoord;",
-        "void main() {",
-        "   gl_FragColor = texture2D(u_curRender, v_texCoord);",
-        "}"
-    ].join("\n");
-    Copy.super.constructor.call(this, fragmentSrc);
-}
-extend(Copy, Trans);
 
 window.Webvs = Webvs;
---
+++ src/effectlist.js
@@ -0,0 +1,132 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: z33m
+ * Date: 6/12/13
+ * Time: 12:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Utility component that copies a texture to the screen
+ * @constructor
+ */
+function Copy() {
+    var fragmentSrc = [
+        "precision mediump float;",
+        "uniform sampler2D u_curRender;",
+        "varying vec2 v_texCoord;",
+        "void main() {",
+        "   gl_FragColor = texture2D(u_curRender, v_texCoord);",
+        "}"
+    ].join("\n");
+    Copy.super.constructor.call(this, fragmentSrc);
+}
+extend(Copy, Trans);
+
+function EffectList(components) {
+    this.components = components;
+    EffectList.super.constructor.call(this);
+}
+extend(EffectList, Component, {
+    swapFrame: true,
+
+    initComponent: function(gl, resolution, analyser) {
+        EffectList.super.initComponent.call(this, gl, resolution, analyser);
+        this._initFrameBuffer();
+
+        var components = this.components;
+        var copyComponent = new Copy();
+
+        // initialize all the components
+        var initPromises = [];
+        for(var i = 0;i < components.length;i++) {
+            var res = components[i].initComponent(gl, resolution, analyser);
+            if(res) {
+                initPromises.push(res);
+            }
+        }
+        copyComponent.initComponent(gl, this.resolution, analyser);
+
+        this.copyComponent = copyComponent;
+        return D.all(initPromises);
+    },
+
+    updateComponent: function(texture) {
+        EffectList.super.updateComponent.call(this, texture);
+        var gl = this.gl;
+
+        // save the current framebuffer
+        var targetFrameBuffer = gl.getParameter(gl.FRAMEBUFFER_BINDING);
+
+        // switch to internal framebuffer
+        gl.bindFramebuffer(gl.FRAMEBUFFER, self.framebuffer);
+        gl.viewport(0, 0, self.resolution.width, self.resolution.height);
+        self._setFBAttachment();
+
+        // render all the components
+        var components = this.components;
+        for(var i = 0;i < components.length;i++) {
+            var component = components[i];
+            gl.useProgram(component.program);
+            if(component.swapFrame) {
+                var oldTexture = self._getCurrentTextrue();
+                self._swapFBAttachment();
+                component.updateComponent(oldTexture);
+            } else {
+                component.updateComponent();
+            }
+        }
+
+        // switch to old framebuffer and copy the data
+        gl.bindFramebuffer(gl.FRAMEBUFFER, targetFrameBuffer);
+        gl.useProgram(copyComponent.program);
+        gl.viewport(0, 0, self.resolution.width, self.resolution.height);
+        copyComponent.updateComponent(self.frameAttachments[self.currAttachment].texture);
+    },
+
+    _initFrameBuffer: function() {
+        var gl = this.gl;
+        var framebuffer = gl.createFramebuffer();
+        var attachments = [];
+        for(var i = 0;i < 2;i++) {
+            var texture = gl.createTexture();
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.resolution.width, this.resolution.height,
+                0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+            var renderbuffer = gl.createRenderbuffer();
+            gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
+            gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, this.resolution.width, this.resolution.height);
+
+            attachments[i] = {
+                texture: texture,
+                renderbuffer: renderbuffer
+            };
+        }
+        this.framebuffer = framebuffer;
+        this.frameAttachments = attachments;
+        this.currAttachment = 0;
+    },
+
+    _setFBAttachment: function() {
+        var gl = this.gl;
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.frameAttachments[this.currAttachment].texture, 0);
+        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this.frameAttachments[this.currAttachment].renderbuffer);
+    },
+
+    _getCurrentTextrue: function() {
+        return this.frameAttachments[this.currAttachment].texture;
+    },
+
+    _swapFBAttachment: function() {
+        this.currAttachment = (this.currAttachment + 1) % 2;
+        this._setFBAttachment();
+    }
+});
+
+window.Webvs.EffectList = EffectList;
