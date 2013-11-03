535a22a4354163d081dbb1143e54378568a8498a
azeem <kr00r4n@gmail.com>
2013-11-03 08:28:11 +0530

    convolution shader code improvement

--- src/trans/Convolution.js
+++ src/trans/Convolution.js
@@ -131,7 +131,7 @@ function ConvolutionProgram(kernel, kernelSize, edgeMode, scale, bias) {
             if(value === 0) {
                 continue;
             }
-            colorSumEq.push("pos = v_position + onePixel * vec2("+(i-mid)+","+(j-mid)+");");
+            colorSumEq.push("pos = corn + texel * vec2("+(i-mid)+","+(j-mid)+");");
             colorSumEq.push(edgeFunc);
             colorSumEq.push("colorSum += texture2D(u_srcTexture, pos) * "+Webvs.glslFloatRepr(value)+";");
         }
@@ -146,7 +146,8 @@ function ConvolutionProgram(kernel, kernelSize, edgeMode, scale, bias) {
         swapFrame: true,
         fragmentShader: [
             "void main() {",
-            "   vec2 onePixel = vec2(1.0, 1.0)/u_resolution;",
+            "   vec2 texel = 1.0/(u_resolution-vec2(1,1));",
+            "   vec2 corn = floor(v_position/texel)*texel;",
             "   vec2 pos;",
             "   vec4 colorSum = vec4(0,0,0,0);",
             colorSumEq.join("\n"),
