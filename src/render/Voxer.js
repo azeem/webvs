39d23adb68a5ba37c109682a4c824f435d8a9b51
Azeem Arshad <azeem.arshadt@gmail.com>
2015-02-11 22:15:30 -0800

    voxer wip

--- src/render/Voxer.js
+++ src/render/Voxer.js
@@ -17,36 +17,71 @@
                 perFrame: "",
                 perObject: ""
             },
-            modelSrc: "cube.obj"
+            modelSrc: "cube.obj",
+            blendMode: "REPLACE"
         },
 
         onChange: {
             code: "updateCode",
-            modelSrc: "updateMode"
+            modelSrc: "updateMode",
+            blendMode: "updateProgram"
         },
 
         init: function() {
             this.updateCode();
             this.updateModel();
+            this.updateProgram();
         },
 
         draw: function() {
             if(!this.inited) {
                 code.init();
-                
+                this.objects = [];
+                for(var i = 0;i < code.n;i++) {
+                    var object3d = {
+                        position: vec3.create(),
+                        quaternion: quat.create(),
+                        scale: vec3.create()
+                    };
+                    object3d.positionCodeRef = this.code.__vec3.length;
+                    this.code.__vec3.push(object3d.position);
+
+                    object3d.scaleCodeRef = this.code.__vec3.length;
+                    this.code.__vec3.push(object3d.scale);
+
+                    object3d.quaternionCodeRef = this.code.__quat.length;
+                    this.code.__quat.push(object3d.quaternion);
+
+                    this.objects.push(object3d);
+                }
             }
 
+            code.perFrame();
             var beat = this.main.analyser.beat;
             code.b = beat?1:0;
-            code.perFrame();
             if(beat) {
                 code.onBeat();
+            }
+
+            for(var i = 0;i < code.n;i++) {
+                var object3d = this.objects[i];
+                code.i = i;
+                code.position = object3d.positionCodeRef;
+                code.quaternion = object3d.quaternionCodeRef;
+                code.scale = object3d.scaleCodeRef;
+                code.perObject();
+                var matrix = mat4.identity(mat4.create());
+                mat4.fromRotationTranslation(matrix, object3d.quaternion, object3d.position);
+                mat4.scale(matrix, matrix, object3d.scale);
+
+                this.program.run(this.parent.fm, null, this.mesh, matrix);
             }
 
         },
 
         updateCode: function() {
             var code = Webvs.compileExpr(this.opts.code, ["init", "onBeat", "perFrame", "perObject"]);
+            code.n = 1;
             code.setup(this.main, this);
             this.inited = false;
             this.code = code;
@@ -61,6 +96,14 @@
                 null,
                 this
             );
+        },
+
+        updateProgram: function() {
+            var program = new VoxerShader(this.gl, this.opts.blendMode);
+            if(this.program) {
+                this.program.destroy();
+            }
+            this.program = program;
         }
     });
 
