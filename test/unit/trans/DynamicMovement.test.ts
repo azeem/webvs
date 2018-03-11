d4a482327d9c5d3e269e51f5c025394a9900445d
Azeem Arshad <azeem.arshadt@gmail.com>
2018-03-11 00:07:01 -0800

    DM Fix Test

--- test/unit/trans/DynamicMovement.test.ts
+++ test/unit/trans/DynamicMovement.test.ts
@@ -1,32 +1,33 @@
+/* tslint:disable:no-unused-expression ban-types */
 import { expect } from "chai";
-import { It, Mock } from "typemoq";
+import { It, Mock, Times } from "typemoq";
 import { IContainer } from "../../../src/Component";
 import CodeInstance from "../../../src/expr/CodeInstance";
 import IMain from "../../../src/IMain";
 import DynamicMovement from "../../../src/trans/DynamicMovement";
 import FrameBufferManager from "../../../src/webgl/FrameBufferManager";
 import RenderingContext from "../../../src/webgl/RenderingContext";
-import ShaderProgram from "../../../src/webgl/ShaderProgram";
+import ShaderProgram, { IShaderOpts } from "../../../src/webgl/ShaderProgram";
 
 describe("DynamicMovement", () => {
-    it("should bind uniforms for codeInstance", () => {
+    it("drawHook should bind uniforms for codeInstance", () => {
         const mockMain          = Mock.ofType<IMain>();
         const mockParent        = Mock.ofType<IContainer>();
         const mockShaderProgram = Mock.ofType<ShaderProgram>();
         const mockCodeInstance  = Mock.ofType(CodeInstance);
         const mockRctx          = Mock.ofType(RenderingContext);
-        // tslint:disable-next-line:ban-types
-        const mockCompileExpr    = Mock.ofType<Function>();
-        const mockGl             = Mock.ofType<WebGLRenderingContext>();
+        const mockCompileExpr   = Mock.ofType<Function>();
+        const mockGl            = Mock.ofType<WebGLRenderingContext>();
 
         mockRctx.setup((x) => x.gl).returns(() => mockGl.object);
         mockMain.setup((x) => x.rctx).returns(() => mockRctx.object);
         mockCompileExpr.setup((x) => x(It.isAny(), It.isAny(), It.isAny(), It.isAny()))
                        .returns(() => ({ codeInst: mockCodeInstance.object }));
 
+        let drawHook: (values: any, gl: WebGLRenderingContext, shader: ShaderProgram) => any;
         // tslint:disable-next-line:only-arrow-functions
-        const mockShaderProgramConstructor = function(rctx, opts) {
-            expect(opts.drawHook).to.be.a("Function");
+        const mockShaderProgramConstructor = function(rctx: RenderingContext, opts: IShaderOpts) {
+            drawHook = opts.drawHook;
             return mockShaderProgram.object;
         };
 
@@ -41,5 +42,8 @@ describe("DynamicMovement", () => {
         }).default as typeof DynamicMovement;
 
         const dmComponent = new DMClass(mockMain.object, mockParent.object, {});
+        expect(drawHook).to.be.ok;
+        drawHook({}, mockGl.object, mockShaderProgram.object);
+        mockCodeInstance.verify((x) => x.bindUniforms(mockShaderProgram.object), Times.once());
     });
 });
